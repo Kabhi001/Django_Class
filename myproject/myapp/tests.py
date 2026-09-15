20d6ee9b0e860f05d3154be58f5a4d63e3900a73
Anurag <anuragp817@gmail.com>
2026-09-15 14:43:24 +0530

    Fix Django test indentation and routes

--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -1,7 +1,35 @@
 from django.test import TestCase
 
-# Create your tests here.
 
 class HomeTest(TestCase):
     def test_home(self):
-        self.assertEqual(2+2,4)
+        self.assertEqual(2 + 2, 4)
+
+
+class HomeViewTest(TestCase):
+    def test_home_view(self):
+        response = self.client.get('/aboutus/')
+        self.assertEqual(response.status_code, 200)
+
+
+class HomeContentTest(TestCase):
+    def test_home_content(self):
+        response = self.client.get('/aboutus/')
+        self.assertContains(response, "About Us")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
